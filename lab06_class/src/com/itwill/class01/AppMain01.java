b26683e9be67f3b9fbce3fc960637030bd5126d1
soLobster <dhwnd0526@gmail.com>
2023-09-15 17:24:33 +0900

    20230915 JavaStudy_ITWILL

--- lab06_class/src/com/itwill/class01/AppMain01.java
+++ lab06_class/src/com/itwill/class01/AppMain01.java
@@ -4,7 +4,22 @@ public class AppMain01 {
 
     public static void main(String[] args) {
         // 클래스 = 데이터 타입.
-
+        Subject subject = new Subject(); //클래스 
+        // 생성자 (constructor). : 클래스의 변수(필드)들을 초기화.        
+        
+        System.out.println(subject.java);
+        System.out.println(subject.sql);
+        System.out.println(subject.javaScript);
+        
+        subject.java = 100;
+        subject.sql = 100;
+        subject.javaScript = 50;
+        
+        System.out.println(subject.java);
+        System.out.println(subject.sql);
+        System.out.println(subject.javaScript);
+        
+        
     }
 
 }
