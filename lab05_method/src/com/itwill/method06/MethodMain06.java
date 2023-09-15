28b8b51c9de2f73d20331c9c83f75e4afd7fd734
soLobster <dhwnd0526@gmail.com>
2023-09-15 11:43:45 +0900

    Java Study

--- lab05_method/src/com/itwill/method06/MethodMain06.java
+++ lab05_method/src/com/itwill/method06/MethodMain06.java
@@ -1,10 +1,33 @@
 package com.itwill.method06;
 
+import java.util.Random;
+
 public class MethodMain06 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        // 메서드 작성 연습.
+        Random random = new Random();
+        
+        int genderCode = random.nextInt(5); //[0,5) 범위의 난수 저장.
+        System.out.println("genderCode : "+genderCode);
+        
+        //parseGenderCode()는 아규먼트가 1or3이면 male, 2or4이면 female, 0인경우 unknown 리턴.
+        String gender = parseGenderCode(genderCode);
+        System.out.println("gender : "+gender);
     }
 
-}
+    public static String parseGenderCode(int num) {
+        String gender = " ";
+        
+        if(num==1||num==3) {
+            gender = "M";
+        } else if(num==2||num==4) {
+            gender = "F";
+        } else {
+            gender = "UnKnown";
+        }
+        
+        return gender;
+    }//end parseGenderCode...
+    
+}//end class...
