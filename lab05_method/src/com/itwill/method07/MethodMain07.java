4cf685c3e2a75a7f851e5dec18a75dcfa1c52351
soLobster <dhwnd0526@gmail.com>
2023-09-15 14:22:19 +0900

    20230915 JavaStudy_ITWILL

--- lab05_method/src/com/itwill/method07/MethodMain07.java
+++ lab05_method/src/com/itwill/method07/MethodMain07.java
@@ -3,7 +3,10 @@ package com.itwill.method07;
 public class MethodMain07 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        // 가변길이인수(variable-length argument):
+        // 메서드를 호출할 때 아규먼트의 개수가 정해져 있지 않은 경우.
+        //ex System.out.printf();
+        System.out.printf("%s는 %d살입니다.\n","mount",25);
 
     }
 
