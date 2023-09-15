2a616291a2441169c94ad648c2ab909e90efd507
soLobster <dhwnd0526@gmail.com>
2023-09-15 17:07:40 +0900

    20230915 JavaStudy_ITWILL

--- lab06_class/src/com/itwill/class01/AppMain01.java
+++ lab06_class/src/com/itwill/class01/AppMain01.java
@@ -3,7 +3,7 @@ package com.itwill.class01;
 public class AppMain01 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        // 클래스 = 데이터 타입.
 
     }
 
---
+++ lab06_class/src/com/itwill/class01/Subject.java
@@ -0,0 +1,14 @@
+package com.itwill.class01;
+
+public class Subject {
+ 
+    
+    // 필드(field). 멤버 변수.
+    int java; // 저장할 수 있는 설계도라 생각.
+    int sql; // 초기화도 안된 상태.
+    int javaScript;
+    
+    
+    
+    
+}
