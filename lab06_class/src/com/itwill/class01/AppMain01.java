570c64436342af6e1bfd950fd4c784797ea61dbd
soLobster <dhwnd0526@gmail.com>
2023-09-15 18:15:29 +0900

    20230915 JavaStudy_ITWILL

--- lab06_class/src/com/itwill/class01/AppMain01.java
+++ lab06_class/src/com/itwill/class01/AppMain01.java
@@ -4,20 +4,27 @@ public class AppMain01 {
 
     public static void main(String[] args) {
         // 클래스 = 데이터 타입.
-        Subject subject = new Subject(); //클래스 
-        // 생성자 (constructor). : 클래스의 변수(필드)들을 초기화.        
+        Subject subject = new Subject(); //생성자 호출. 객체 생성. 인스턴스화. 
+        // 생성자 (constructor). : 클래스의 변수(필드)들을 초기화.(initialize).
         
         System.out.println(subject.java);
         System.out.println(subject.sql);
-        System.out.println(subject.javaScript);
+        System.out.println(subject.js);
         
         subject.java = 100;
         subject.sql = 100;
-        subject.javaScript = 50;
+        subject.js = 50;
         
-        System.out.println(subject.java);
-        System.out.println(subject.sql);
-        System.out.println(subject.javaScript);
+        System.out.println("java: "+subject.java);
+        System.out.println("sql: "+subject.sql);
+        System.out.println("javaScript: "+subject.js);
+        
+        
+        Subject sub2 = new Subject(100,50,10);
+        System.out.println(sub2.java);
+        System.out.println(sub2.sql);
+        System.out.println(sub2.js);
+        
         
         
     }
--- lab06_class/src/com/itwill/class01/Subject.java
+++ lab06_class/src/com/itwill/class01/Subject.java
@@ -6,9 +6,32 @@ public class Subject {
     // 필드(field). 멤버 변수.
     int java; // 저장할 수 있는 설계도라 생각.
     int sql; // 초기화도 안된 상태.
-    int javaScript;
+    int js;
     
+    //생성자(constructor)
+    //객체를 생성하고. 객체의 필드들을 초기화하는 역할. 담당....!
+    //생성자의 이름은 반드시 클래스의 이름과 같아야한다.
+    //메서드를 작성하는 방법과 비슷한다. 다른점 하나!
+    //리턴타입을 쓰지 않는다....!
     
+    //아규먼트 없는 생성자.
+    public Subject() {
+        // 아무 코드를 작성하지 않아도 됨.
+        // 하지만 블록 괄호는 생략할 수 없음.
+    }
+    
+    //아규먼트를 갖는 생성자. - 아규먼트의 값들로 필드를 초기화한다.
+    public Subject(int java,int sql,int js) { //this!
+       this.java = java;
+       this.sql =sql;
+       this.js =js;
+    }
+    
+    //기본 생성자 (default constructor).
+    //아규먼트를 전달받지 않는 생성자.
+    //일반적으로 필드 타입의 기본값으로 필드들을 초기화 한다.
+    // - 정수 기본값: 0, 실수 기본값: 0.0, boolean 기본값 false 
+    // 나머지 참조타입 -> null (ex String)
     
     
 }
