3f6f11b43c0378d21a995657232b7f8c0c468937
soLobster <dhwnd0526@gmail.com>
2023-09-18 17:13:31 +0900

    20230918 JavaStudy_ITWILL

--- lab06_class/src/com/itwill/class04/AppMain04.java
+++ lab06_class/src/com/itwill/class04/AppMain04.java
@@ -6,12 +6,14 @@ public class AppMain04 {
     public static void main(String[] args) {
         //Score 객체를 기본 생성자를 사용해서 생성.
         Score score1 = new Score();
+        Student std = new Student();
         System.out.println(score1.korean);
         System.out.println(score1.english);
         System.out.println(score1.math);
         System.out.println(score1.getTotal());
         System.out.println(score1.getMean());
-        
+       // std.studentInfo(); null 포인트 익셉션 발생....
+        // 스코어가 선언이 안되어 있기 때문
         System.out.println("==============");
         
         //Score 객체를 아규먼트를 갖는 생성자를 이용해서 생성.
@@ -40,7 +42,7 @@ public class AppMain04 {
         Student std2 = new Student(24,"james",score2);
         std2.studentInfo();
         
-        Student std3 = new Student(30,"silva",new Score(40,60,70));
+        Student std3 = new Student(6,"silva",new Score(40,60,70));
         std3.studentInfo();
         
     }
--- lab06_class/src/com/itwill/class04/Student.java
+++ lab06_class/src/com/itwill/class04/Student.java
@@ -13,7 +13,10 @@ public class Student {
     //생성자
     //기본 생성자.
     public Student() {
-        
+      //필드 int studentNo = 0;
+      //필드 String studentName = null;
+      //필드 Score score  = null;
+            
     }
     
     public Student(int studentNo, String StudentName, Score score) {
@@ -26,19 +29,18 @@ public class Student {
     //학생의 정보를 출력하는 메서드
     //번호, 이름, 국어점수, 영어점수, 수학점수, 총점, 평균 출력.
     
-
-    
-    
     public void studentInfo() {
         System.out.println("student No = "+studentNo);
-        System.out.println("Student Name = "+StudentName);
-        System.out.println("student Korean Score = "+score.korean);
-        System.out.println("student English Score = "+score.english);
-        System.out.println("student Math Score = "+score.math);
-        System.out.println("student Exam Total Score = "+score.getTotal());
-        System.out.println("student Exam Mean Score = "+score.getMean());
-        
+        System.out.println("student Name = "+StudentName);
+        if(score != null) {
+            System.out.println("student Korean Score = "+score.korean);
+            System.out.println("student English Score = "+score.english);
+            System.out.println("student Math Score = "+score.math);
+            System.out.println("student Exam Total Score = "+score.getTotal());
+            System.out.println("student Exam Mean Score = "+score.getMean());
+        } else {
+            System.out.println("출력 불가. score == null ");
+        }
     }
-    
 }
 
