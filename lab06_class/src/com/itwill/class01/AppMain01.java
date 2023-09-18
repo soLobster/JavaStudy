6e5c0ff06577b59f4ddb050fc993460573baf763
soLobster <dhwnd0526@gmail.com>
2023-09-18 11:23:44 +0900

    20230918

--- lab06_class/src/com/itwill/class01/AppMain01.java
+++ lab06_class/src/com/itwill/class01/AppMain01.java
@@ -4,29 +4,52 @@ public class AppMain01 {
 
     public static void main(String[] args) {
         // 클래스 = 데이터 타입.
-        Subject subject = new Subject(); //생성자 호출. 객체 생성. 인스턴스화. 
+        Subject sub1 = new Subject(); //생성자 호출. 객체 생성. 인스턴스화. 
         // 생성자 (constructor). : 클래스의 변수(필드)들을 초기화.(initialize).
         
-        System.out.println(subject.java);
-        System.out.println(subject.sql);
-        System.out.println(subject.js);
+        // stack -> local variable
+        // heap -> object
+        // method(static) -> byte code, static(variable, method)
         
-        subject.java = 100;
-        subject.sql = 100;
-        subject.js = 50;
         
-        System.out.println("java: "+subject.java);
-        System.out.println("sql: "+subject.sql);
-        System.out.println("javaScript: "+subject.js);
+        System.out.println(sub1.java);
+        System.out.println(sub1.sql);
+        System.out.println(sub1.js);
+        
+        sub1.java = 100;
+        sub1.sql = 100;
+        sub1.js = 50;
+        
+        System.out.println("java: "+sub1.java);
+        System.out.println("sql: "+sub1.sql);
+        System.out.println("javaScript: "+sub1.js);
         
         
         Subject sub2 = new Subject(100,50,10);
         System.out.println(sub2.java);
         System.out.println(sub2.sql);
-        System.out.println(sub2.js);
+        System.out.println(sub2.js);  //(.) 참조 연산자. -> 주소값을 찾아간다
         
+        System.out.println("===============");
         
+        //함수형 프로그래밍 방법(functional programming).
+        int total1 = getTotal(sub1);
+        int total2 = getTotal(sub2);
+        System.out.println(total1);
+        System.out.println(getTotal(sub2));
+        System.out.println(total2);
         
+        //객체지향 프로그래밍(object-oriented programming).
+        System.out.println("sub1 total = "+sub1.getTotal());
+        System.out.println("sub1 mean = "+sub1.getMean());
+        
+        System.out.println("sub1 total = "+sub2.getTotal());
+        System.out.println("sub1 mean = "+sub2.getMean());
+    }//end main...
+    
+    //subject 타입의 객체를 아규먼트로 받아서 세 과목의 총점을 반환하는 기능.
+    public static int getTotal(Subject subject) {
+        return subject.java+subject.js+subject.sql;
     }
 
-}
+}//end class Appmain01
--- lab06_class/src/com/itwill/class01/Subject.java
+++ lab06_class/src/com/itwill/class01/Subject.java
@@ -1,5 +1,7 @@
 package com.itwill.class01;
 
+//클래스 = 데이터(필드) + 초기화(생성자) + 기능(메서드) => 데이터 타입.
+
 public class Subject {
  
     
@@ -9,10 +11,13 @@ public class Subject {
     int js;
     
     //생성자(constructor)
-    //객체를 생성하고. 객체의 필드들을 초기화하는 역할. 담당....!
+    //객체를 생성(메모리를 확보)하고. 객체의 필드들을 초기화하는 역할. 담당....!
     //생성자의 이름은 반드시 클래스의 이름과 같아야한다.
     //메서드를 작성하는 방법과 비슷한다. 다른점 하나!
     //리턴타입을 쓰지 않는다....!
+    //클래스를 작성할 때 생성자를 하나도 정의하지 않으면, 자바 컴파일러가 "기본 생성자"를 자동으로 만들어준다.
+    //클래스를 작성할 때 생성자를 직접 작성하면, 자바 컴파일러는 기본 생성자를 자동으로 만들지 않음.
+    //생성자 오버로딩(Over Loading) 생성자를 여러개 정의하는 것.
     
     //아규먼트 없는 생성자.
     public Subject() {
@@ -22,10 +27,24 @@ public class Subject {
     
     //아규먼트를 갖는 생성자. - 아규먼트의 값들로 필드를 초기화한다.
     public Subject(int java,int sql,int js) { //this! 파라미터가 달라야 한다 메서드 오버로딩
-       this.java = java;
+       this.java = java; //이 경우 this 생략 불가....(필드와 파라미터를 구분해야한다)
        this.sql =sql;
        this.js =js;
     }
+    
+    //메서드(기능)
+    //1.총점
+    //2.평균값.
+    
+    public int getTotal() {
+        return this.java + this.sql + this.js; //필드는 메서드가 사용할 수 있다.
+    }
+    
+    public double getMean() {
+        return (double) this.getTotal()/ 3; //이 this는 생략가능하다.
+        //지역변수와 필드가 혼동되지 않을 경우 this 생략 가능.
+    }
+    
     
     //기본 생성자 (default constructor).
     //아규먼트를 전달받지 않는 생성자.
@@ -34,4 +53,4 @@ public class Subject {
     // 나머지 참조타입 -> null (ex String)
     
     
-}
+} //end class subject
