48ca5b9a7c7af73868e7a8236e1356647ff73ca3
soLobster <dhwnd0526@gmail.com>
2023-09-19 16:03:16 +0900

    20230919

--- lab06_class/src/com/itwill/modifier04/Member.java
+++ lab06_class/src/com/itwill/modifier04/Member.java
@@ -2,6 +2,7 @@ package com.itwill.modifier04;
 //회원.....
 public class Member {
     
+    //private 타입 캡슐화.....
     private String memberId; //읽기전용
     private String memberPwd; //읽/쓰 가능.
     //필드....
@@ -9,19 +10,19 @@ public class Member {
     public Member(String memberId, String memberPwd) {
         this.memberId = memberId;
         this.memberPwd = memberPwd;
-    }
+    }//Member 생성자.....
     
     
     //getter /setter 
     public String getMemberId() {
         return this.memberId;
-    }
+    } //getter.....
     
     public String getMemeberPwd() {
         return this.memberPwd;
-    }
+    } //getter.....
     
     public void setMemberPwd(String memberPwd) {
         this.memberPwd = memberPwd;
-    }
+    }//setter.....
 }
