c6c89ee82f644f8d976f82c3e5e25fa1594da1d7
soLobster <dhwnd0526@gmail.com>
2023-09-20 11:58:29 +0900

    20230920

--- lab06_class/src/com/itwill/modifier08/ModifierMain08.java
+++ lab06_class/src/com/itwill/modifier08/ModifierMain08.java
@@ -8,7 +8,29 @@ package com.itwill.modifier08;
  * final 필드는 
  * 1) 선언과 동시에 초기화를 하거나. 
  * 2) 반드시 아규먼트를 갖는 생성자를 선언해서 명시적으로 초기화해야 함.
+ */
+
+
+/**
+ * 객체(object)...프로그램에서 만들 대상.(비유) 붕어빵.
+ * 클래스(class)...객체를 생성하기 위한 프로그램 코드, 설계도.....클래스 이름은 데이터 타입이다. (비유) 붕어빵 틀.
+ * 인스턴스(instance)...메모리에 생성된 객체를 인스턴스라 한다....(비유) 만들어진 붕어빵....
+ * instantiate...인스턴스화하다. 객체를 생성하다..... 생성자 호출.
  * 
+ * 클래스의 멤버(필드, 메서드).
+ * 1) instance(필드,메서드) 멤버. 
+ *  1 static 수식어가 사용되지 않은 멤버
+ *  2 객체를 생성한 후에(생성자를 호출한 다음) 참조 변수를 이용해서 사용하는 멤버.....
+ *  3 JRE(java runtime environment)이 사용하는 메모리 공간 중에서 힙(heap)에 생성되는 변수....
+ *  
+ * 2) static(필드,메서드)(정적) 멤버.
+ *  1 static 수식어가 사용된 멤버
+ *  2 객체를 생성하지 않아도 사용할 수 있는 멤버.
+ *  3 클래스 이름을 이용해서 사용하는 멤버.ex) Math.PI , Math.sqrt(),.....
+ *  4 힙에 생성되지 않고 JRE가 사용하는 메모리 공간 중에서 메서드 영역에 생성되는 변수.
+ *  5 프로그램 main() 메서드가 호출되기 전에 프로그램 로딩 시점에 메모리에 생성.
+ *  6 static 메서드는 static 필드와 메서드만 접근 가능.
+ *  
  */
 
 public final class ModifierMain08 { //final class 
@@ -20,7 +42,7 @@ public final class ModifierMain08 { //final class
     private final int n;
     //생성자..
     public ModifierMain08(int n) {
-        this.n = n;
+        this.n = n; //final 필드를 명시적으로 초기화....
     }
     
     public static void main(String[] args) {
@@ -28,8 +50,23 @@ public final class ModifierMain08 { //final class
         final int x = 1;
         //x = 2; cannot assign 할당할 수 없음....! 컴파일 에러
         //final 지역 변수는 값을 할당(저장)할 수 없음....
-      
         
+        //스태틱 멤버 사용방법.... 클래스 이름을 접두사로 사용한다....객체 생성 전에도 사용가능....
+        System.out.println(Test.staticVar);
+        Test.staticVar = 1;
+        System.out.println(Test.staticVar);
+        Test.printVariables2();
+        
+        //System.out.println(Test.printVariables2());
+        //인스턴스 멤버 사용방법....참조 변수를 이용한다. 객체를 생성한 후에 사용이 가능하다.
+        
+        System.out.println();
+        
+        Test t = new Test();
+        System.out.println(t.instanceVar);
+        t.instanceVar = 100;
+        t.printVariables();
+        //System.out.println(t.printVariables());
         
     }
 
---
+++ lab06_class/src/com/itwill/modifier08/Test.java
@@ -0,0 +1,25 @@
+package com.itwill.modifier08;
+
+public class Test {
+    int instanceVar;//instance 필드...
+    static int staticVar; // static 필드...이텔릭체...-> static....
+    //사용하는 방법이 다르다....
+    
+    
+    //instance 메서드...(static이 붙어 있지 않는 메서드)
+    //참조변수가 있어야 호출가능한 메서드.
+    public void printVariables() {
+        System.out.println("----인스턴스 메서드----");
+        System.out.println("instanceVar >> "+instanceVar); //this.instanceVar
+        System.out.println("staticVar >> "+staticVar); //Test.staticVar
+        System.out.println("-----------------------");
+    }
+    //static 메서드 수식어는 무조건 리턴타입 앞에 와야한다....
+    //클래스 이름만으로 호출 가능한 메서드.
+    public static void printVariables2() {
+        System.out.println("------static 메서드--------");
+//      System.out.println("instanceVar >> "+instanceVar);//스테틱 메서드에선 스테텍이 아닌것(=인스턴스 멤버)을 접근하려하면 안된다...
+        System.out.println("staticVar >> "+staticVar); //스테틱 필드 접근 가능...!!! / 스테틱 변수만 사용 가능하다.
+        System.out.println("---------------------------");
+    }
+}
