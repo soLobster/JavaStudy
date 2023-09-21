97d57acadd955a181e1f57c83ceec03ed3028005
soLobster <dhwnd0526@gmail.com>
2023-09-21 18:05:33 +0900

    20230921 JavaStudy

--- lab07_inheritance/src/com/itwill/inheritance02/SubClass.java
+++ lab07_inheritance/src/com/itwill/inheritance02/SubClass.java
@@ -2,9 +2,13 @@ package com.itwill.inheritance02;
 
 public class SubClass extends SuperClass{
 
-
+    //클래스에서 생성자를 1개도 작성하지 않은 경우
+    //자바 컴파일러 기본 생성자를 만들어 줌.
+    //하위 클래스 생성자에서는 명시적인 호출이 없는 경우 상위 클래스의 기본 생성자가 호출 된다.
+    //이런 경우에 만약에 상위 클래스에 기본 생성자가 없으면, 컴파일 에러가 발생한다... 
+    
     public SubClass() {
-        super(0);
+        super(0); // -> 아규먼트를 갖는 상위 클래스 생성자를 명시적으로 호출해야만 함.
     }
 
 
