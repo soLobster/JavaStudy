0aadb728cc54dfcf4ce1ceeb8d7de7d005b422e0
soLobster <dhwnd0526@gmail.com>
2023-09-21 17:56:21 +0900

    20230921 JavaStudy

---
+++ lab07_inheritance/src/com/itwill/inheritance02/SubClass.java
@@ -0,0 +1,11 @@
+package com.itwill.inheritance02;
+
+public class SubClass extends SuperClass{
+
+    public SubClass(int a) {
+        super(a);
+        // TODO Auto-generated constructor stub
+    }
+
+  
+}
--- lab07_inheritance/src/com/itwill/inheritance02/SuperClass.java
+++ lab07_inheritance/src/com/itwill/inheritance02/SuperClass.java
@@ -4,12 +4,14 @@ public class SuperClass {
 
     private int a;
 
-    //public SuperClass() {} //기본 생성자..
+    //      public SuperClass() {
+    //        this(0);
+    //      } //기본 생성자..
 
     public SuperClass(int a) {
         //this(); -> 위의 기본 생성자를 호출하는 의미...[overLoading] 기본 생성자 호출.
-        this.a = a;
-        //this의 의미 -> SuperClass(자기 자신) -> Private int a의 주소.
+        this.a = a; //this의 의미 -> SuperClass(자기 자신) -> Private int a의 주소.
+
     }
 
 
