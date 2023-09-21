bc3636da3d734e02f5914fc2518d527b4543b461
soLobster <dhwnd0526@gmail.com>
2023-09-21 14:21:16 +0900

    20230921 JavaStudy

--- lab07_inheritance/src/com/itwill/inheritance01/BasicTv.java
+++ lab07_inheritance/src/com/itwill/inheritance01/BasicTv.java
@@ -50,7 +50,7 @@ public class BasicTv {
 
     //Tv의 기능 메서드로 만들자...!
     public boolean powerOnOff() {
-        if(powerOn==true) {
+        if(powerOn) { //boolean 변수는 굳이 비교 연산자를 쓸 필요가 없당.
             powerOn = false;
             //System.out.println("TV 꺼짐");
         } else {
@@ -63,36 +63,48 @@ public class BasicTv {
     public int channelUp() {
         if(channel < MAX_CHANNEL) {
             channel++;
-        } else if(channel == MAX_CHANNEL) {
+        } else /*if(channel == MAX_CHANNEL)*/ {
             channel = MIN_CHANNEL;
         }
+        //System.out.println("현재 채널 >> "+channel);
         return channel;
     }//end of channelUp...
 
     public int channelDown() {
         if(MAX_CHANNEL >= channel && channel > MIN_CHANNEL) {
             channel--;
-        } else if(channel == MIN_CHANNEL) {
+        } else /* if(channel == MIN_CHANNEL) */{
             channel = MAX_CHANNEL;
         }
+        //System.out.println("현재 채널 >> "+channel);
         return channel;
     }//end of channelDown....
 
     public int volumeUp() {
         if(volume < MAX_VOLUME) {
             volume++;
-        } else if(volume==MAX_VOLUME){
+        } else /*if(volume == MAX_VOLUME)*/ {
             volume = MAX_VOLUME;
         }
+        //System.out.println("현재 볼륨 >> "+volume);
         return volume;
     }//end of volumeUp...
 
     public int volumeDown() {
         if(MAX_VOLUME >= volume && volume > MIN_VOLUME) {
             volume--;
-        }else if(volume==MIN_VOLUME) {
+        }else /* if(volume == MIN_VOLUME) */ {
             volume = MIN_VOLUME;
         }
+        //System.out.println("현재 볼륨 >> "+volume);
         return volume;
     }//end of volumeDown...
+    
+    
+    public String toString() {
+        return "BasicTv(powerOn = "+this.powerOn
+                +", channel = "+this.channel
+                +", volume = "+this.volume
+                +")";
+    }//end of toString.
 }//end of BasicTv class..
--- lab07_inheritance/src/com/itwill/inheritance01/InheritanceMain01.java
+++ lab07_inheritance/src/com/itwill/inheritance01/InheritanceMain01.java
@@ -6,46 +6,72 @@ public class InheritanceMain01 {
         // TODO Auto-generated method stub
         BasicTv tv = new BasicTv();
 
-        System.out.println("전원이 켜져 있는가? >> "+tv.isPowerOn());
+        System.out.println(tv.toString());
+        //System.out.println("전원이 켜져 있는가? >> "+tv.isPowerOn());
         //System.out.println("전원이 켜져 있는가? >> "+tv.isPowerOn());
         tv.powerOnOff();
-        System.out.println("지금 상태는?? >> "+tv.isPowerOn());
+        //System.out.println("지금 상태는?? >> "+tv.isPowerOn());
+        System.out.println(tv.toString());
         tv.powerOnOff();
-        System.out.println("지금 상태는?? >> "+tv.isPowerOn());
+        System.out.println(tv.toString());
+        //System.out.println("지금 상태는?? >> "+tv.isPowerOn());
+        System.out.println();
         System.out.println("===channel Test===");
-        System.out.println("현재 tv 채널은??>> "+tv.getChannel()); //0
+        System.out.println();
+        //System.out.println("현재 tv 채널은?? >> "+tv.getChannel()); //0
         tv.channelUp(); //++
-        System.out.println("현재 tv 채널은??>> "+tv.getChannel()); //1
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 채널은?? >> "+tv.getChannel()); //1
         tv.channelUp(); //++
-        System.out.println("현재 tv 채널은??>> "+tv.getChannel()); //2
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 채널은?? >> "+tv.getChannel()); //2
         tv.channelUp(); //++ 
-        System.out.println("현재 tv 채널은??>> "+tv.getChannel()); //0
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 채널은?? >> "+tv.getChannel()); //0
+
+        tv.channelUp();
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 채널은?? >> "+tv.getChannel()); //1
+        tv.channelUp();
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 채널은?? >> "+tv.getChannel()); //2
+        tv.channelDown();
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 채널은?? >> "+tv.getChannel()); //1
+        tv.channelDown();
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 채널은?? >> "+tv.getChannel()); //0
+        tv.channelDown();
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 채널은?? >> "+tv.getChannel()); //2
+
+        System.out.println();
+      
         
-        tv.channelUp();
-        System.out.println("현재 tv 채널은??>> "+tv.getChannel()); //1
-        tv.channelUp();
-        System.out.println("현재 tv 채널은??>> "+tv.getChannel()); //2
-        tv.channelDown();
-        System.out.println("현재 tv 채널은??>> "+tv.getChannel()); //1
-        tv.channelDown();
-        System.out.println("현재 tv 채널은??>> "+tv.getChannel()); //0
-        tv.channelDown();
-        System.out.println("현재 tv 채널은??>> "+tv.getChannel()); //2
-    
+        System.out.println();
         System.out.println("===volume Test===");
-        System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume());
+        System.out.println();
+        //System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume()); //0
         tv.volumeUp();
-        System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume());
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume()); //1
         tv.volumeUp();
-        System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume());
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume()); //2
         tv.volumeUp();
-        System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume()); //변동 없어야함.
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume()); //변동 없어야함.
         tv.volumeDown();
-        System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume());
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume()); //1
         tv.volumeDown();
-        System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume());
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume()); //0
         tv.volumeDown();
-        System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume()); //변동 없어야함..
+        System.out.println(tv.toString());
+        //System.out.println("현재 tv 볼륨은?? >> "+tv.getVolume()); //변동 없어야함..
+        System.out.println();
+  
     }//end of Main....
 
 }//end of InheritanceMain01 class..
