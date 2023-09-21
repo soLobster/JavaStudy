b6fb2308bc2b2741b6cb91b728f381139f111a83
soLobster <dhwnd0526@gmail.com>
2023-09-21 17:02:39 +0900

    20230921 JavaStudy

--- lab07_inheritance/src/com/itwill/inheritance02/InheritanceMain02.java
+++ lab07_inheritance/src/com/itwill/inheritance02/InheritanceMain02.java
@@ -3,8 +3,31 @@ package com.itwill.inheritance02;
 public class InheritanceMain02 {
 
     public static void main(String[] args) {
+        //Person 타입 객체를 생성.....기본 생성자를 사용해서 생성....
         
+        Person p1 = new Person();
+        System.out.println("p1 이름 >> "+p1.getName()); //private으로 감춰진 name을 getter 메서드로 가져온다...
 
+        System.out.println("--super--");
+        
+        //Person 타입 객체를 아규먼트를 갖는 생성자를 사용해서 생성....
+        Person p2 = new Person("LeBron James");
+        System.out.println("p2 이름 >> "+p2.getName());
+        
+        
+        System.out.println("==inheritance==");
+        Student s1 = new Student(); //객체가 연속된 공간에 저장된다...(메모리)....
+        System.out.println("s1 이름 >> "+s1.getName()); //superclass Person에서 상속받은 getter...
+        System.out.println("s1 학교 >> "+s1.getSchoolName()); //subclass Student에서 작성한 getter...
+        
+        s1.setName("Lebron James Jr");
+        s1.setSchoolName("LA Lakers");
+        System.out.println("======");
+        System.out.println("s1 이름 >> "+s1.getName());
+        System.out.println("s1 학교 >> "+s1.getSchoolName());
+        
+        Student s2 = new Student("denver Nuggets");
+        System.out.println("s2 학교 >> "+s2.getSchoolName());
     }
 
 }
--- lab07_inheritance/src/com/itwill/inheritance02/Student.java
+++ lab07_inheritance/src/com/itwill/inheritance02/Student.java
@@ -6,13 +6,37 @@ public class Student extends Person {
     //필드 확장...
     private String schoolName;
 
+    
+    /*
+     * 생성자
+     * 상위(부모) 타입의 객체가 먼저 생성되어 있어야 하위(자식) 타입 객체도 생성될 수 있음.
+     * 생성자 호출 순서 -> 상위 클래스 생성자 -> 하위 클래스 생성자 호출
+     * 하위 클래스 생성자에서 상위 클래스 생성자를 명시적으로 호출하지 않으면..
+     * 상위 클래스의 기본 생성자가 자동으로 호출됨..
+     * 하위 클래스 생성자에서 명시적으로 상위 클래스 생성자를 호출하려면 super() 키워드를 사용.
+     * 상위 클래스의 아규먼트(들)을 갖는 생성자는 자동으로 호출되지 않음.
+     * 상위 클래스의 아규먼트(들)을 갖는 생성자를 이용하려면 명시적으로 super(...)를 호출해야 함...
+     * super(...) 호출은 생성자 안에서 가장 먼저 작성되어야 함.
+     */
+    
     //생성자 생성...고민 곰곰...
     public Student() {
+        super(); //상위 클래스의 "기본 생성자" 명시적 호출....
+        //super의 호출은 생성자 안에서 가장 먼저 작성되어야한다의 사례.
+        //부모 먼저 만들고 자식이 와야한다.
+        
         System.out.println("Student() 호출");
+        //super(); 컴파일 오류 난다.
     } //기본 생성자....
+    //heap에 null 타입으로 초기화가 된다..
+    //name.....schoolname.....
     
-    
-    
+    //1) 아규먼트 1개 (학교이름)를 갖는 생성자...
+    public Student(String schoolName) {
+        super();//생략 가능 (omit).
+        this.schoolName = schoolName;
+    }//아규먼트를 갖는 생성자 생성.....!
+
     //getter, setter.
     public String getSchoolName() {
         return schoolName;
