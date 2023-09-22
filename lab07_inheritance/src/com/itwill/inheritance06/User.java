a53a83bf6e1989b001244eada399e127cc96b1f4
soLobster <dhwnd0526@gmail.com>
2023-09-22 17:51:55 +0900

    20230922 JAVASTUDY

---
+++ lab07_inheritance/src/com/itwill/inheritance06/User.java
@@ -0,0 +1,29 @@
+package com.itwill.inheritance06;
+
+public class User {
+//field
+    private String id;
+    private String pwd;
+    
+    
+    //ToDo
+    //생성자 기본생성자 + 아규먼트 2개짜리 생성자
+    
+    public User() {}
+    
+    public User(String id, String pwd) {
+        this.id = id;
+        this.pwd =pwd;
+    } //fin....
+    
+    
+    
+    //ToDo = toString 재정의...
+    
+    //ToDo = equals 재정의...- Id만 일치하면 같은 객채.
+    //일치 true / false.
+    
+    //ToDO hashCode 재정의...
+    
+    
+}
