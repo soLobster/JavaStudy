08621025ec696c45e1f282a7f0c3ae1fce2cee0c
soLobster <dhwnd0526@gmail.com>
2023-09-25 17:53:02 +0900

    20230925

---
+++ lab08_mvc/src/com/itwill/model/Member.java
@@ -0,0 +1,43 @@
+package com.itwill.model;
+
+//MVC 아키텍처에서 Model 역할 담당하는 클래스 - 데이터 클래스 
+//VO(Value Object) DTO(Data Transfer Object)..
+
+public class Member {
+    //field
+    private String id;
+    private String pwd;
+
+    public Member() {}
+    //기본 생성자...
+    
+    public Member(String id, String pwd) {
+        this.id = id;
+        this.pwd = pwd;
+    }//아규먼트 생성자...
+
+    
+    //getter/setter
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+    
+    @Override
+    public String toString() {
+        
+        return "Member [id = "+this.id+", pwd = "+this.pwd+"]";
+    }//over_ride toString.
+    
+}
---
+++ lab08_mvc/src/com/itwill/view/MemberMain.java
@@ -0,0 +1,13 @@
+package com.itwill.view;
+
+//MVC(Model-View-Controller) 아키텍쳐에서 view 역할 담당 클래스. - UI (입/출)
+
+public class MemberMain {
+
+    public static void main(String[] args) {
+        System.out.println("*** 회원 관리 프로그램 ***");
+
+
+    }
+
+}
