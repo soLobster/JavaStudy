64e2408ae93f540ef6795556e0a4fc76d6cd247f
soLobster <dhwnd0526@gmail.com>
2023-09-25 14:01:22 +0900

    20230925

---
+++ lab07_inheritance/src/com/itwill/interface01/InterfaceMain01.java
@@ -0,0 +1,25 @@
+package com.itwill.interface01;
+
+/*
+ * 인터페이스(interface)
+ * 사용하는 목적....팀이나 회사간의 분업이나 협업을 하기 위해서 
+ * 메서드들의 signature(메서드의 선언부)를 약속하기 위한 규칙, 규약
+ * 인터페이스가 가질 수 있는 멤버 -> 
+ * 1) public static final 필드. -> 상수만 가질 수 있다.
+ *    "public static final" 수식어는 보통 생략을 한다.
+ * 2) public abstract 메서드. "public abstract" 수식어는 보통 생략.
+ * 인터페이스를 작성할 때는 "interface" 키워드로 선언.
+ * 인터페이스 자체는 객체를 생성할 수 없고, 인터페이스를 "구현"하는 클래스를 작성해서 객체를 생성할 수 있다.
+ * -> class 클래스이름 implements 인터페이스 {.....} 
+ * 클래스는 단일 상속만 가능하지만, 인터페이스 구현은 개수 제한이 없다.
+ * 
+ */
+
+public class InterfaceMain01 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
