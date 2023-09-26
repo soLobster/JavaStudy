01169bed37814ac0670ccd5799ebb9b8593bf66c
soLobster <dhwnd0526@gmail.com>
2023-09-26 16:53:24 +0900

    20230926

--- lab_contact/src/com/itwill/ver02/model/Contact.java
+++ lab_contact/src/com/itwill/ver02/model/Contact.java
@@ -1,5 +1,55 @@
 package com.itwill.ver02.model;
 
-public class Contact {
+//MVC 아키텍처에서 Model 역할 담당하는 클래스 - 데이터 클래스 
+//VO(Value Object) DTO(Data Transfer Object)..
 
+
+public class Contact {
+    //field
+    private String name;
+    private String phone;
+    private String email;
+    
+    //constructor
+    public Contact() {} // 기본 생성자....
+    
+    public Contact(String name, String Phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }// 아규먼트를 갖는 생성자....
+
+    
+    //getter, setter...
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String toString() {
+        return "Contact ( name = "+this.name+
+                ", phone = "+this.phone+
+                ", email = "+this.email+
+                " )";
+    }//toString over_ride...
+    
 }
