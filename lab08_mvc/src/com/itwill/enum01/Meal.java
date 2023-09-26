327ecfdef9e27a3df244acc0510ee74513d7925c
soLobster <dhwnd0526@gmail.com>
2023-09-26 15:22:00 +0900

    20230926

---
+++ lab08_mvc/src/com/itwill/enum01/Meal.java
@@ -0,0 +1,14 @@
+package com.itwill.enum01;
+
+public class Meal {
+    //상수들을 정의...
+    public static final int BREAKFAST = 0;
+    public static final int LUNCH = 1;
+    public static final int DINNER = 2;
+    
+    //private 생성자
+    private Meal() {}
+    
+    
+         
+}
