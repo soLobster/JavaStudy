78c8597e630dc6088607abcaeb0445253b037959
soLobster <dhwnd0526@gmail.com>
2023-10-05 11:53:00 +0900

    20231005

--- lab10_api/src/com/itwill/wrapper/WrapperEx.java
+++ lab10_api/src/com/itwill/wrapper/WrapperEx.java
@@ -1,9 +1,42 @@
 package com.itwill.wrapper;
 
+/*
+ * Wrapper 클래스 : 자바의 기본 데이터 타입의 값을 하나만 저장하는(감싸고 있는) 그런 클래스.
+ * 기본데이터타입: boolean, byte, short, int, long, char, float, double
+ * wrapper 클래스 : Boolean, Byte , Short, Integer, Long, Character, Float, Double
+ * - java.lang 패키지에 있기 때문에, import 문장 없이 사용이 가능하다.
+ * - 목적: 
+ *  1) 기본 데이터 타입의 데이터를 이용하는 메서드를 제공.
+ *  2) 상속 다형성 이용 가능.
+ *  3) generic 클래스를 이용하기 위해서.(List, Set, Map)를 이용하기 위해서.
+ *  
+ */
+
+
 public class WrapperEx {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        // Integer 타입 객체 : int 값을 Integer 객체로 생성.
+        Integer number1 = Integer.valueOf(1111); //Integer 객체 임을 주의!!
+        System.out.println(number1);
+        
+        Integer number2 = Integer.valueOf("100"); //String 값을 Integer 객체로 생성.
+        System.out.println(number2);
+        
+        int x = Integer.parseInt("123"); //String 값을 int 값으로 변환.
+        System.out.println(x);
+        
+        
+        //auto-Boxing 원시타입에서 래퍼클래스 타입으로 자동 변환하는것.
+        //int 값을 자동으로 Integer 타입 객체로 생성.
+        Integer number3 = 3;//문법적으로 말이 안됨...허용하는것임 
+        System.out.println(number3);
+        
+        //auto-Unboxing...
+        //Integer 타입 객체에서 int 값을 자동으로 추출.
+        int subtract = number1 - number3;
+        System.out.println(subtract);
+        
 
     }
 
---
+++ lab11_collection/src/com/itwill/list01/LIstMain01.java
@@ -0,0 +1,28 @@
+package com.itwill.list01;
+
+/*
+ * Collection<E>
+ * |_________List<E>
+ *      |________ArrayList<E>, LinkedList<E>, .....
+ *      
+ * 리스트(list)의 특징.
+ * 1. 값들을 저장하는 순서가 중요. (index를 가지고 있다.)
+ * 2. 값들을 저장할때마다 인덱스는 자동으로 증가한다. - 저장 공간이 자동으로 증가.
+ * 3. 리스트 중간에 있는 값을 삭제하면 인덱스가 자동으로 재배열됨.
+ * 4. 같은 값들을 여러번 저장할 수 있다. - Set<E>과 다른 점.
+ * 5. List<E> Elements (원소) 리스트가 저장하는 원소의 데이터 타입을 의미한다.
+ *    (주의) 리스트는 클래스 타입의 객체만 저장할 수 있음. 기본 타입 데이터는 저장할 수 없다.
+ *    Ex) List<Integer> list = new(); (O)
+ *        List<int> list = new(); (X)
+ *        
+ * 
+ */
+
+public class LIstMain01 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
