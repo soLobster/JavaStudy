171bceee0b20de076e6e1185e956a74d014fdf0e
soLobster <dhwnd0526@gmail.com>
2023-10-05 15:12:58 +0900

    20231005

--- lab11_collection/src/com/itwill/list03/ListMain03.java
+++ lab11_collection/src/com/itwill/list03/ListMain03.java
@@ -11,7 +11,7 @@ public class ListMain03 {
         //1.names에서 다섯 글자 이상인 문자열을 대문자로 변환해서 저장하는 ArrayList를 만들고 출력.
         ArrayList<String> fiveNames = new ArrayList<String>();
         for(String x : names) {
-            if(x.length()>5) {
+            if(x.length()>=5) {
                 fiveNames.add(x.toUpperCase());
             }
         }
@@ -24,7 +24,21 @@ public class ListMain03 {
             numOfNames.add(names.get(i).length());
         }
         System.out.println(numOfNames);
-    
+
+        System.out.println();
+        
+        List<Integer> codes = Arrays.asList(0,1,0,1,1,0);
+        //codes 의 원소가 0이면 "남성", 1이면 "여성"을 저장하는 ArrayList를 만들고 출력
+        ArrayList<String> gender = new ArrayList<String>();
+        for(int i=0; i<codes.size(); i++) {
+            if(codes.get(i)==0) {
+                gender.add("남성");
+            } else {
+                gender.add("여성");
+            }
+        }System.out.println(gender);
+        
+        
     }//end of main
 
 }//end of class
