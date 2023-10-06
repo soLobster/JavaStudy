d6774f79713f99ee5922ea603c01be2134d64b1e
soLobster <dhwnd0526@gmail.com>
2023-10-06 11:08:04 +0900

    20231006

---
+++ lab11_collection/src/com/itwill/map01/MapMain01.java
@@ -0,0 +1,26 @@
+package com.itwill.map01;
+
+/*
+ * Map<K, V>
+ * |________HashMap<K,V>, TreeMap<K,V>....
+ * 
+ * Map<K,V> Key와 Value 키와 값의 쌍으로 구성된 데이터를 저장하는 자료 구조.
+ * 1. Key: 중복된 값을 가질 수 없다. -> 하나의 값을 찾기 위한 인덱스와 비슷한 역할.
+ *  - 데이터를 저장/검색/수정/삭제할 때 값을 찾기 위한 용도.
+ *  - 연속된 값을 가질 필요는 없음.
+ * 2. Value: 중복된 값을 가질 수 있음.
+ * 
+ * HashMap<K,V> 검색을 빠르게 할 수 있는 Map.
+ * TreeMap<K,V> 정렬을 빠르게 할 수 있는 Map.
+ * 
+ */
+
+
+public class MapMain01 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
