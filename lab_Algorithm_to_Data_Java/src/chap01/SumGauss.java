81b55287f17ffd470e25947585677786f6bc4631
soLobster <dhwnd0526@gmail.com>
2023-10-09 09:31:11 +0900

    20231009

---
+++ lab_Algorithm_to_Data_Java/src/chap01/SumGauss.java
@@ -0,0 +1,11 @@
+package chap01;
+
+import java.util.Scanner;
+
+public class SumGauss {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+
+}
