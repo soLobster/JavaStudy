3dde67dc64a1742b228598a16d4cd19ca8026bde
soLobster <dhwnd0526@gmail.com>
2023-10-10 11:17:31 +0900

    20231010

---
+++ lab12_file/src/com/itwill/file01/FileMain01.java
@@ -0,0 +1,31 @@
+package com.itwill.file01;
+
+
+/*
+ * 입출력 스트림 
+ * 프로그램에서 값을 입력받거나 출력하는 통로.
+ * ex) System.in -> 입력 스트림 객체...
+ *     System.out -> 출력 스트림 객체...
+ *     
+ *     프로그램 <== InputStream <== 입력장치(키보드,마우스,터치스크린,파일,....)
+ *     프로그램 ==> OutputStream ==> 출력장치(모니터, 프린터, 프로젝터, 파일....)
+ * 
+ *     프로그램 <== FileInputStream <== 파일
+ *     프로그램 ==> FileOutputStream ==> 파일
+ *     
+ *     
+ *     java.io.InputStream : 프로그램이 데이터를 읽어오는 통로.
+ *     |______FileInputStream : 프로그램이 파일에서 데이터를 읽어오는 통로.
+ *     
+ *     1. FileInputStream(FIS) 객체 생성 
+ *     2. FIS 객체의 read 관련 메서드를 사용.
+ *     3. FIS 객체를 close.
+ */
+
+public class FileMain01 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
