c2d2d899157ccf96c59145282286a41602fc3e31
soLobster <dhwnd0526@gmail.com>
2023-10-10 12:16:16 +0900

    20231010

--- lab12_file/src/com/itwill/file01/FileMain01.java
+++ lab12_file/src/com/itwill/file01/FileMain01.java
@@ -1,5 +1,9 @@
 package com.itwill.file01;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /*
  * 입출력 스트림 
@@ -19,13 +23,53 @@ package com.itwill.file01;
  *     
  *     1. FileInputStream(FIS) 객체 생성 
  *     2. FIS 객체의 read 관련 메서드를 사용.
- *     3. FIS 객체를 close.
+ *     3. FIS 객체를 close!
+ *     
+ *     java.io.OutputStream : 프로그램에서 데이터를 Write하는 통로다.
+ *     |_____FileOutputStream : 프로그램에서 파일에 데이터를 쓰는 통로.
+ *     
+ *     1. FileOutputStream(FOS) 객체 생성.
+ *     2. FOS 객체의 write 관련 메서드를 사용.
+ *     3. FOS 객체를 close.
  */
 
 public class FileMain01 {
 
     public static void main(String[] args) {
-        
-    }
-    
-}
+        String fileName = "data/hello.txt"; //읽을 파일 경로, 이름.
+        String copyFile = "data/hello_copy.txt"; //복사할 파일 경로, 이름.
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        try { //runtimeException을 상속받지 않기에 try - catch 문을 써야한다.
+            // 1. FIS 객체 생성.
+            in = new FileInputStream(fileName);
+            //FOS 객체 생성.
+            out = new FileOutputStream(copyFile);
+
+            boolean run = true;
+
+            while(run) {
+                // 2. read 메서드 사용.
+                int read = in.read();
+
+                if(read == -1){ //end of file (EOF)
+                    run =false;    
+                }
+                //System.out.print((char) read);
+                // 2. FOS의 write 메서드 사용.
+                out.write(read); 
+            }
+            System.out.print("파일 복사 성공");
+        } catch (Exception e) {
+            e.printStackTrace(); //예외이름, 메시지, 발생 위치를 출력.
+        } finally {
+            try {
+                in.close(); //FIS 객체를 닫음.
+                out.close();//FOS 객체를 닫음.
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    } // end main
+} // end class
