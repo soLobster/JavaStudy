f4afc2c09ac0ffa101e668e221ec0af8d68bdeb5
soLobster <dhwnd0526@gmail.com>
2023-10-11 14:16:57 +0900

    20231011

---
+++ lab12_file/src/com/itwill/file09/FileMain09.java
@@ -0,0 +1,28 @@
+package com.itwill.file09;
+
+/*
+ * 파일, 폴더(디렉토리) 관리 - 정보, 생성, 삭제.
+ * 
+ * 현재 작업 디렉토리(CWD) 현재 프로그램(프로세서)가 실행되는 디렉토리 위치.
+ * - 이클립스에서 자바 프로그램을 실행(run as java Application) 할 때, 작업 디렉토리 위치는 프로젝트 폴더. 
+ * 
+ * 절대 경로(absolute Path).
+ * - 시스템의 루트부터 파일또는 폴더가 있는 위치까지를 찾아가는 방식
+ * ex) c:\workspace\lab_java\lab12_file\data\ansi.txt (WIN)
+ * ex) /users/itwill/documents/ansi.txt (MAC,LINUX)
+ * 
+ * 상대 경로(relative Path).
+ * - 현재 작업 디렉토리(CWD, Current Working Directory)를 기준으로 파일 또는 폴더 위치를 찾아가는 방식.
+ * ex) data\ansi.txt (win)
+ * ex) data/ansi.txt (mac)
+ * 
+ */
+
+public class FileMain09 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
