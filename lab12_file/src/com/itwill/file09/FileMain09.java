b2cc7492e49310b3e2f230965dc62ec26b5ebb36
soLobster <dhwnd0526@gmail.com>
2023-10-11 15:08:20 +0900

    20231011

--- lab12_file/src/com/itwill/file09/FileMain09.java
+++ lab12_file/src/com/itwill/file09/FileMain09.java
@@ -31,18 +31,32 @@ public class FileMain09 {
         // Java Runtime이 프로그램을 실행하고 있는 현재 디렉토리 찾기.
         String cwd = System.getProperty("user.dir");
         System.out.println(cwd);
-        
+
         String path = "C:\\work_space\\lab_java\\JavaStudy\\lab12_file"; //Windows에서의 절대 경로 표현.
         System.out.println(path);
-        
+
         String path2 = "data\\ansi.txt"; //windows에서의 상대 경로.
         //현재는 os 구분없이 "data/ansi.txt"로 표현해도 됨.
-        
+
         System.out.println(path2);
-        
+
         String path3 = "data"+File.separator+"ansi.txt";
-        
+
         System.out.println(path3);
+
+        //File 클래스:
+        //파일(txt, jpg, mp4, pptx,...) 과 폴더(디렉토리)에 관련된 기능들을 가지고 있는 클래스.
+        //File 객체 생성 - 파일(파일&폴더)을 관리하는 객체 생성.
+        File f = new File(path3);
+        System.out.println(f); // File 클래스는 toString 메서드를 재정의.
+        System.out.println("경로 : "+f.getPath());
+        //-> File 객체를 생성할 떄 상대경로로 생성한 경우, getPath()는 상대경로를 리턴
+        //-> File 객체를 생성할 떄 절대경로로 생성한 경우, getPath()는 절대경로를 리턴
+        System.out.println("절대 경로 : "+f.getAbsolutePath());
+        System.out.println("생성 여부 : "+f.exists());
+        System.out.println("파일 여부 : "+f.isFile());
+        System.out.println("폴더 여부 : "+f.isDirectory());
+
     }
 
 }
