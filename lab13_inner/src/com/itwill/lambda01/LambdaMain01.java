0d7b20d4e3895f38154ead0ad4c67bba4d977322
soLobster <dhwnd0526@gmail.com>
2023-10-13 11:24:44 +0900

    20231013

--- lab13_inner/src/com/itwill/lambda01/LambdaMain01.java
+++ lab13_inner/src/com/itwill/lambda01/LambdaMain01.java
@@ -39,8 +39,24 @@ public class LambdaMain01 {
         System.out.println("result = "+result);
         
         //4.람다 표현식 (Lambda expression) - 함수형 표기법.
-        result = calc.calculate((x,y) -> x/y);
+        result = calc.calculate((x,y) -> x/y); // 축약형.
+        //result = calc.calculate((double x, double y) -> {return x/y;}); 원래는 이런 형태다.
         System.out.println("result = "+result);
+        
+        /*
+         * 람다 표현식:
+         * 클래스 선언과 객체 생성을 동시에 하는 익명 클래스를 간단히 작성하기 위한 문법.
+         * 함수형 인터페이스 타입 객체만 람다 표현식으로 작성할 수 있습니다. 
+         * 
+         * 람다 표현식 문법 : (파라미터 선언) -> {메서드 코드;}
+         * - 람다 표현식의 파라미터 선언에서는 변수 타입을 생략 할 수 있음.
+         * - 람다 표현식에서 메서드 본체의 문장이 하나만 있는 경우에는 {}와 ; 을 생략할 수 있다.
+         * - 람다 표현식이 return 문장만 있는 경우에는 {}, return, ; 을 모두 생략하고, 리턴값만 표기.
+         *   ex) (x,y) -> x + y 
+         * - 람다 표현식에서 파라미터가 1개만 있는 경우에는 괄호()도 생략 가능하다.
+         *   ex) x -> 2*x+3 
+         */
+        
     }//main
 
 }
