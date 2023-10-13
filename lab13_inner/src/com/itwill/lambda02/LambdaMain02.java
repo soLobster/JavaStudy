5873899a4163a60309ff4365c33216d9e27fa6dd
soLobster <dhwnd0526@gmail.com>
2023-10-13 12:15:44 +0900

    20231013

--- lab13_inner/src/com/itwill/lambda02/LambdaMain02.java
+++ lab13_inner/src/com/itwill/lambda02/LambdaMain02.java
@@ -1,11 +1,12 @@
 package com.itwill.lambda02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LambdaMain02 {
 
-    
+
     public List<Object> filter(List<Object> list, MyFilter filter) {
         List<Object> result = new ArrayList<Object>();
         for(Object x : list) {
@@ -15,11 +16,30 @@ public class LambdaMain02 {
         }//for
         return result;
     }//filter method
-    
-    
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
+
+    public static void main(String[] args) {
+        // LambdaMain02 타입 객체 생성
+        LambdaMain02 app = new LambdaMain02();
+
+        List<Object> numbers = Arrays.asList(1,2,3,4,5,6,7);
+        System.out.println(numbers);
+
+        // numbers의 원소들 중에서 짝수들로만 이루어진 리스트를 만들고 출력.
+        List<Object> evens = app.filter(numbers, new MyFilter() {
+            @Override
+            public boolean test(Object x) { //MyFilter(FunctionalInterface) - abstract method - Override
+                return(Integer) x % 2 == 0;
+            }
+        });
+
+        System.out.println(evens);
+
+        // numbers의 원소들 중에서 홀수들로만 이루어진 리스트를 만들고 출력.
+        // 람다 표현식을 사용.
+
+        List<Object> odds = app.filter(numbers, x -> (Integer) x % 2 == 1);
+        System.out.println(odds);
     }
 
 }
--- lab13_inner/src/com/itwill/lambda02/MyFilter.java
+++ lab13_inner/src/com/itwill/lambda02/MyFilter.java
@@ -2,7 +2,7 @@ package com.itwill.lambda02;
 
 @FunctionalInterface
 public interface MyFilter {
-    
+    //abstract method
     boolean test(Object x);
     
 }
