fde71a27fdf7a77c87d416be777cb11c11421efa
soLobster <dhwnd0526@gmail.com>
2023-10-13 18:03:18 +0900

    20231013

--- lab13_inner/src/com/itwill/lambda04/LambdaMain04.java
+++ lab13_inner/src/com/itwill/lambda04/LambdaMain04.java
@@ -18,9 +18,11 @@ public class LambdaMain04 {
                 );
         
         // 1. 모든 직원들의 정보를 한줄에 한 명씩 출력.
+        System.out.println("1. 모든 직원들의 정보를 한줄에 한 명씩 출력.");
         employees.forEach((x) -> System.out.println(x));
-        System.out.println("==================");
+        
         // 2. 개발(1,2팀)에서 일하는 사원들의 급여 합계를 출력.
+        System.out.println("2. 개발(1,2팀)에서 일하는 사원들의 급여 합계를 출력.");
         List<Double> codingOneTwo = employees.stream()
                 .filter((x) -> x.getDept().equals("개발1팀") || x.getDept().equals( "개발2팀"))
                 .map((x) -> x.getSalary()).toList();
@@ -33,13 +35,14 @@ public class LambdaMain04 {
         System.out.println("개발 1,2팀 급여의 합 >> " + codingTeamsalSum);
         
         // 3. 개발팀에서 일하는 사원들의 급여 평균을 출력.
-        double salMean = 0;
-        int codingTeamempCount = codingOneTwo.size();
-        double codingTeamsalMean = codingTeamsalSum/codingTeamempCount;
+        System.out.println("3. 개발팀에서 일하는 사원들의 급여 평균을 출력.");
+        double codingTeamsalMean = 0;
+        int codingTeamEmpCount = codingOneTwo.size();
+        codingTeamsalMean = codingTeamsalSum/codingTeamEmpCount;
         System.out.println("개발 1,2팀 급여의 평균 >> " +codingTeamsalMean);
        
         // 4. 직급이 "사원"인 직원들의 급여 합계, 평균을 출력.
-        
+        System.out.println("4. 직급이 \"사원\"인 직원들의 급여 합계, 평균을 출력.");
         List<Double> titleSawon = employees.stream()
                 .filter((x) -> x.getEmpTitile().equals("사원"))
                 .map((x) -> x.getSalary())
@@ -54,15 +57,15 @@ public class LambdaMain04 {
         
         double saWonSalMean = saWonSalSum/saWonCount;
         
-        System.out.printf("사원들의 연봉 총합 >> %.1f 과 평균 >> %.1f",saWonSalSum,saWonSalMean).println();
+        System.out.printf("사원들의 연봉 총합 = %.1f 과 평균 = %.1f",saWonSalSum,saWonSalMean).println();
         
         // 5. 급여가 1_000을 초과하는 사원들의 정보를 한줄에 한 명씩 출력.
-        
+        System.out.println("5. 급여가 1_000을 초과하는 사원들의 정보를 한줄에 한 명씩 출력.");
         List<Employee> allEmployeesOver1000 = employees.stream().filter((x)-> x.getSalary() > 1_000).toList();
-        System.out.println("급여가 1_000을 초과하는 사원들 목록");
         allEmployeesOver1000.forEach((x)->System.out.println(x));
         
         // 6. 개발 1팀 사원들의 급여를 10% 인상하고, 인상한 급여의 평균을 출력.
+        System.out.println("6. 개발 1팀 사원들의 급여를 10% 인상하고, 인상한 급여의 평균을 출력.");
         List<Double> codingOneTeam = employees.stream().filter((x)-> x.getDept().equals("개발1팀")).map((x)-> x.getSalary()*1.1).toList();
         //System.out.println(codingOneTeam);
         double codingOneTeamSalSum = 0;
@@ -72,9 +75,10 @@ public class LambdaMain04 {
 
         int codingOneTeamempCount = codingOneTeam.size();
         double codingOneTeamSalMean = codingOneTeamSalSum/codingOneTeamempCount;
-        System.out.println("개발 1팀 인상한 급여의 평균 >> "+codingOneTeamSalMean);
+        System.out.println("개발 1팀의 인상한 급여의 평균 >> "+codingOneTeamSalMean);
         
         // 7. 직원들 중에서 대리는 몇명 있을까? count 결과 3. 
+        System.out.println("7. 직원들 중에서 대리는 몇명 있을까?");
         System.out.println("직원들 중 대리는 몇명?? >> "+employees.stream().filter((x)-> x.getEmpTitile().equals("대리")).toList().size()+"명");
     }
 
