acb06915d3dcc4a6f5b2a018244f2d63e0ebb5e2
soLobster <dhwnd0526@gmail.com>
2023-10-16 18:09:05 +0900

    20231016 Java Swing

---
+++ lab15_swing/src/com/itwill/swing04/AppMain04.java
@@ -0,0 +1,43 @@
+package com.itwill.swing04;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AppMain04 {
+
+    private JFrame frame;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    AppMain04 window = new AppMain04();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public AppMain04() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 450, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
