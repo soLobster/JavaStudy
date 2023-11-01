96d60dda244e17cb45e4e8d32088e2dc6d8ec161
soLobster <dhwnd0526@gmail.com>
2023-11-01 18:16:37 +0900

    Java Project First Clear

--- lab_mini_project_gym/src/com/itwill/gym/GymJdbc.java
+++ lab_mini_project_gym/src/com/itwill/gym/GymJdbc.java
@@ -2,7 +2,7 @@ package com.itwill.gym;
 
 public interface GymJdbc {
     
-    String URL = "jdbc:oracle:thin:@192.168.20.13:1521:xe";
+    String URL = "jdbc:oracle:thin:@1.1.1.11:1521:xe";
     String USER = "scott";
     String PASSWORD = "tiger";
 
